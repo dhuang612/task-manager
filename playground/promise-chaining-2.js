5a9d4e92230874b3c9d305fa9618c7dc95b088fe
dhuang612 <dhuang@madisoncollege.edu>
2019-10-22 12:42:06 -0400

    refactored code to use async / await

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,21 +1,31 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task')
-/*
-Goal mess around with promise chaining.
-mongoosejs.com/docs/queries.html
-use
-findByIdAndDelete()
-create promise-chaining-2.js
-load in mongoose and task model
-remove a given task by id
-get and print the total number of incomplete tasks
 
-*/
+// Task.findByIdAndDelete('5dab412084377c20709aea32').then((task)=> {
+//     return Task.countDocuments({completed: false})
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e);
+// })
+const SearchByIdAndRemove = async (id) => {
+   const task = await Task.findByIdAndDelete(id)
+   const count = await Task.countDocuments({completed: false})
+   return count;
+}
 
-Task.findByIdAndDelete('5dab412084377c20709aea32').then((task)=> {
-    return Task.countDocuments({completed: false})
-}).then((result) => {
-    console.log(result)
+SearchByIdAndRemove('5da9ffaeab6ebb12066cccc1', false).then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e);
 })
+/*
+Goal use async/await
+
+1. create deleteTaskAndCount as an async function
+    - Accept id of task to remove
+2. Use await to delete task and count up incomplete tasks
+3. Return the count
+4. Call the function and attach then/catch to log results
+5. test work 
+*/
---
+++ playground/promise-chaining.js
@@ -0,0 +1,22 @@
+require('../src/db/mongoose');
+const User = require('../src/models/user');
+
+// User.findByIdAndUpdate('5dab3e89271f4d1fc524003a', {age: 1}).then((user)=>{
+//     return User.countDocuments({age:1})
+// }).then((result)=> {
+//     console.log(result);
+// }).catch((e) => {
+//     console.log(e);
+// })
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, {age})
+    const count = await User.countDocuments({age})
+    return count;
+}
+
+updateAgeAndCount('5dab3e89271f4d1fc524003a', 1).then((count)=> {
+    console.log(count)
+}).catch((e)=> {
+    console.log(e)
+})

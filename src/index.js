10845f49267f2686ba8e0cd8441b5611a5de54e0
dhuang612 <dhuang@madisoncollege.edu>
2019-10-25 16:30:27 -0400

    tested out express middleware

--- src/index.js
+++ src/index.js
@@ -7,6 +7,19 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next)=> {
+//     if(req.method === "GET"){
+//          res.send('Get requests are disabled');
+//     }
+//     next();
+// })
+
+app.use((req, res, next)=> {
+    res.status(503).send('Currently in maintenance please check back later')
+  
+})
+
+
 app.use(express.json());
 // Loads in the routes for the user db
 app.use(userRouter);

e9da89f4de9a4ba63c2645b07d12290ad00fd1d8
dhuang612 <dhuang@madisoncollege.edu>
2019-10-27 11:08:25 -0400

    reviewed code and resolved error

--- src/models/user.js
+++ src/models/user.js
@@ -55,7 +55,7 @@ const userSchema = new mongoose.Schema({
 
 userSchema.methods.generateAuthToken = async function(){
     const user = this;
-    const token = jwt.sign({_id: user._id.toString()}, 'thisisageneratedtoken');
+    const token = jwt.sign({_id: user._id.toString() }, 'thisisageneratedtoken');
 
     user.tokens = user.tokens.concat({token})
     await user.save();

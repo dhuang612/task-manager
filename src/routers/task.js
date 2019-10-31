9428f99fda1d78c750dd9a24c817de19ca8a5740
dhuang612 <dhuang@madisoncollege.edu>
2019-10-31 11:27:25 -0400

    updating query options

--- src/routers/task.js
+++ src/routers/task.js
@@ -11,18 +11,32 @@ router.post('/tasks',auth, async (req, res) => {
         owner: req.user._id
     })
     try{
-        task.save();
+        await task.save();
         res.status(201).send(task);
     }catch(e){
         res.status(400).send(e)
     }
 });
 
-
+// Get /tasks?completed=true
+// Get /tasks?limit= 
 router.get('/tasks',auth ,async (req, res ) => {
+    const match = {}
+    
+    if(req.query.completed){
+        match.completed = req.query.completed === 'true'
+    }
+
     try{
-        const tasks = await Task.find({owner: req.user._id})
-        res.send(tasks);
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            options:{
+                limit:parseInt(req.query.limit),
+                skip:parseInt(req.query.skip)
+            }
+        }).execPopulate();
+        res.send(req.user.tasks);
     }catch(e){
         res.status(500).send();
     }

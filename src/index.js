da5277c5c84730d4804e23408c05c4da9a8e0764
dhuang612 <dhuang@madisoncollege.edu>
2019-11-01 11:13:18 -0400

    adding in endpoint for file uploading

--- src/index.js
+++ src/index.js
@@ -7,18 +7,20 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use((req, res, next)=> {
-//     if(req.method === "GET"){
-//          res.send('Get requests are disabled');
-//     }
-//     next();
-// })
 
-// app.use((req, res, next)=> {
-//     res.status(503).send('Currently in maintenance please check back later')
-  
-// })
+const multer = require('multer');
 
+const upload = multer({
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    },
+    
+});
+
+app.post('/upload', upload.single('upload'), (req,res) => {
+    res.send()
+})
 
 app.use(express.json());
 // Loads in the routes for the user db
@@ -29,16 +31,3 @@ app.use(taskRouter);
 app.listen(port, () => {
     console.log(`Server is up on port: ${port}`);
 });
-
-const Task = require('./models/task');
-const User = require('./models/user');
-// const main = async () => {
-//     // const task = await Task.findById('5db7192771d87e15d901df57');
-//     // await task.populate('owner').execPopulate();
-//     // console.log(task.owner)
-//     const user = await User.findById('5db7181f41290715900b379a');
-//     await user.populate('tasks').execPopulate();
-//     console.log(user.tasks);
-// }
-
-// main()

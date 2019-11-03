7890ccbee769f7b38477530633c1b83e7e9b9c7b
dhuang612 <dhuang@madisoncollege.edu>
2019-11-03 12:25:55 -0500

    added in route for user avatars

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const User = require('../models/user');
 const auth = require('../middleware/auth');
 const multer = require('multer');
+const sharp = require('sharp');
 const router = new express.Router();
 
 router.post('/users', async (req, res) => {
@@ -96,7 +97,9 @@ const upload = multer({
 })
 
 router.post('/users/me/avatar',auth ,upload.single('avatar'),async (req, res) =>{
-    req.user.avatar = req.file.buffer;
+    const buffer = await sharp(req.file.buffer).resize({width:250, height:250}).png().toBuffer()
+
+    req.user.avatar = buffer
     await req.user.save()
     res.send();
 }, (error, req, res, next)=>{
@@ -107,6 +110,19 @@ router.delete('/users/me/avatar', auth , async (req, res) => {
     req.user.avatar = undefined;
     await req.user.save();
     res.send();
+});
+
+router.get('/users/:id/avatar', async (req, res) =>{
+    try{
+        const user = await User.findById(req.params.id)
+        if(!user || !user.avatar){
+            throw new Error()
+        }
+        res.set('Content-Type', 'image/png')
+        res.send(user.avatar);
+    }catch(e){
+        res.status(404).send();
+    }
 })
 
 module.exports = router;

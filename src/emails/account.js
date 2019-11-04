8b68864e56ca7f6e4c10a2cce78e421f3a7c3b57
dhuang612 <dhuang@madisoncollege.edu>
2019-11-04 14:57:07 -0500

    sent emails for welcoming and saying goodbye to users

--- src/emails/account.js
+++ src/emails/account.js
@@ -7,9 +7,27 @@ const sendgridAPIKey = process.env.SENDGRID_API_KEY;
 
 sgMail.setApiKey(sendgridAPIKey);
 
-sgMail.send({
-    to: 'dhuang684@gmail.com',
-    from: 'dhuang684@gmail.com',
-    subject: 'Test message',
-    text: 'This is a test.'
-})
+const sendWelcomeEmail = (email, name)=> {
+    sgMail.send({
+        to: email,
+        from: 'Dhuang684@gmail.com',
+        subject: 'Thanks for joining',
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+    })
+}
+
+const sendCancelEmail = (email, name) =>{
+    sgMail.send({
+        to: email,
+        from: 'dhuang684@gmail.com',
+        subject: 'Sorry to see you go!',
+        text: `We hope you have enjoyed our service ${name}, please let us know if there is anything we could have done to have kept you.`
+    })
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancelEmail
+
+}
+

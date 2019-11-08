cdef2ca787f0116eef2e982dff7ba3d9c2480aa0
dhuang612 <dhuang@madisoncollege.edu>
2019-11-08 11:23:42 -0500

    creating basic express unit tests

--- test/user.test.js
+++ test/user.test.js
@@ -1,6 +1,21 @@
 const request = require('supertest');
 //express setup for test suite
 const app = require('../src/app');
+//require in the user db
+const User = require('../src/models/user');
+
+const userOne = {
+    name: "test user",
+    email: "testemail@email.com",
+    password: "Gr3enP@ss"
+}
+
+beforeEach(async () =>{
+    //clear the db
+    await User.deleteMany();
+    //create a user
+    await new User(userOne).save();
+})
 
 test('Should signup a new user', async ()=>{
     //.post = the express route
@@ -9,4 +24,18 @@ test('Should signup a new user', async ()=>{
         email: "dhuang@email.com",
         password: "R3dP@ss77!"
     }).expect(201)
+})
+
+test('Should login user', async () =>{
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
+})
+
+test('should not login nonexistent user', async () =>{
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: ''
+    }).expect(400)
 })
